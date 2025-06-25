fc524fda26dd86ccd5161bacaf06b5f8c5d04f21
Ivan Rygaev <irygaev@gmail.com>
2025-06-25 12:35:24 +0200

    Update for Utrecht 2025 paper

--- monster_model_test.R
+++ monster_model_test.R
@@ -2,12 +2,12 @@ library(tidyverse)
 library(ggplot2)
 library(jsonlite)
 
-json <- read_json("sample.json")
-
 #actions <- c('jump_over', 'wave', 'attack', 'throw_rock')
 actions <- c('overall')
 states <- c('agent_falls', 'patient_falls')
 utterances <- c('np', 'pro', 'zero')
+
+json <- read_json("sample.json")
 
 speaker_data = data.frame(action=character(), state=character(), utterance=character(), count=integer())
 
@@ -47,7 +47,7 @@ speaker_data <- speaker_data %>%
     mutate(state = factor(state, levels=states)) %>%
     mutate(utterance = factor(utterance, levels=utterances))
 
-speaker_gold = read.csv("speaker_gold.csv")
+speaker_gold = read.csv("speaker_gold2.csv")
     
 speaker_gold <- speaker_gold %>%
     mutate(action = factor(action, levels=actions)) %>%
@@ -55,14 +55,34 @@ speaker_gold <- speaker_gold %>%
     mutate(utterance = factor(utterance, levels=utterances)) %>%
     filter(action %in% actions)
 
+speaker_gold %>%
+    group_by(action, state, utterance) %>%
+    summarize(n = sum(count)) %>%
+    mutate(percent = n / sum(n)) %>%
+    ggplot(aes(x=factor(state, labels=c("agent falls", "patient falls")), y=percent, fill=factor(utterance, levels = c("zero", "pro", "np")))) +
+    geom_bar(stat="identity", color="black") + 
+    #facet_grid(~action) +
+    geom_text(aes(label = ifelse(state == "patient_falls" & utterance == "zero", "", sprintf("%0.2f", percent))), vjust = 1.1, size = 5, colour = "black", position = "stack") +
+    labs(x = "State", y="Percentage", fill="Utterance") +
+    scale_fill_grey(start = 0.9, end = 0.5) +
+    #scale_x_discrete(labels = c("familiar", "suprising", "post-surpr.")) +
+    theme_bw() +
+    theme(text = element_text(size = 14))
+ggsave("fig-production.pdf", width = 4, height = 3.7, units = "in")
+
 speaker_data %>%
     ggplot(aes(x=count)) +
     #geom_histogram() +
     geom_density() +
     geom_point(data = speaker_gold, aes(x = count, y = 0, color = "red")) +
-    facet_grid(action ~ state + utterance) +
-    ylim(0, 0.2)
-    
+    facet_grid(factor(state, labels=c("agent falls", "patient falls")) ~ factor(utterance, levels=c("zero", "pro", "np"))) +
+    guides(color="none") +
+    labs(x = "Counts", y="Probability density") +
+    ylim(0, 0.12) +
+    theme_bw() +
+    theme(text = element_text(size = 14))
+ggsave("fig-production-model.pdf", width = 8, height = 3, units = "in")
+
 
 
 json <- read_json("sample.json")
@@ -106,7 +126,7 @@ listener_data <- listener_data %>%
     mutate(utterance = factor(utterance, levels=utterances)) %>%
     mutate(state = factor(state, levels=states))
 
-listener_gold = read.csv("listener_gold.csv")
+listener_gold = read.csv("listener_gold2.csv")
 
 listener_gold <- listener_gold %>%
     mutate(action = factor(action, levels=actions)) %>%
@@ -114,11 +134,31 @@ listener_gold <- listener_gold %>%
     mutate(state = factor(state, levels=states)) %>%
     filter(action %in% actions)
 
+listener_gold %>%
+    group_by(action, utterance, state) %>%
+    summarize(n = sum(count)) %>%
+    mutate(percent = n / sum(n)) %>%
+    ggplot(aes(x=factor(utterance, levels=c("zero", "pro")), y=percent, fill=factor(state, labels=c("agent falls", "patient falls")))) +
+    geom_bar(stat="identity", color="black") + 
+    #facet_grid(~action) +
+    geom_text(aes(label = sprintf("%0.2f", percent)), vjust = 1.1, size = 5, colour = "black", position = "stack") +
+    labs(x = "Utterance", y="Percentage", fill="State") +
+    scale_fill_grey(start = 0.8, end = 0.5) +
+    #scale_x_discrete(labels = c("familiar", "suprising", "post-surpr.")) +
+    theme_bw() +
+    theme(text = element_text(size = 14))
+ggsave("fig-perception.pdf", width = 4, height = 3.7, units = "in")
+
 listener_data %>%
     ggplot(aes(x=count)) +
     #geom_histogram() +
     geom_density() +
     geom_point(data = listener_gold, aes(x = count, y = 0, color = "red")) +
-    facet_grid(action ~ utterance + state) +
-    ylim(0, 0.2)
+    facet_grid(factor(utterance, levels = c("zero", "pro")) ~ factor(state, labels=c("agent falls", "patient falls"))) +
+    guides(color="none") +
+    labs(x = "Counts", y="Probability density") +
+    ylim(0, 0.06) +
+    theme_bw() +
+    theme(text = element_text(size = 14))
+ggsave("fig-perception-model.pdf", width = 8, height = 3, units = "in")
 
